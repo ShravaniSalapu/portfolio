3f35f91764055c5cb519f46381dc4f69188219c3
ShravaniSalapu <130199411+ShravaniSalapu@users.noreply.github.com>
2025-04-03 15:10:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var Typed=new Typed(".text",{
+    strings:["Frontend Developer","Aspiring Software Engineer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+var Typef=new Typef(".text2",{
+    string:["Aspring Software Engineer!!",""],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
